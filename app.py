5f52cd90e1b3ed6806c39afeb35fb54b561a9404
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2025-06-28 13:28:05 +0530

    stable version:1

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+import joblib
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    model = joblib.load('model.pkl')
+    prediction = model.predict([features])
+    output = prediction[0]
+    result = "⚠️ High Risk of Heart Failure" if prediction == 1 else "✅ Low Risk of Heart Failure"
+    return render_template('result.html', prediction_text=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
