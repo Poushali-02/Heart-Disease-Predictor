06fad4d2a09cde0a79bfcc5e5c418115baa43439
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2025-06-28 21:09:43 +0530

    stable version:3

--- app.py
+++ app.py
@@ -8,12 +8,17 @@ def main():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    features = [float(x) for x in request.form.values()]
-    model = joblib.load('model.pkl')
-    prediction = model.predict([features])
-    color_class = 'yes' if prediction == 1 else 'no'
-    result = "High Risk of Heart Failure" if prediction == 1 else "Low Risk of Heart Failure"
-    return render_template('result.html', prediction_text=result, color_class=color_class)
+    try:
+        features = [float(x) for x in request.form.values()]
+        model = joblib.load('model.pkl')
+        prediction = model.predict([features])[0]
+        color_class = 'yes' if prediction == 1 else 'no'
+        result = "High Risk of Heart Failure" if prediction == 1 else "Low Risk of Heart Failure"
+        return render_template('result.html', prediction_text=result, color_class=color_class)
+
+    except Exception as e:
+        return render_template('result.html', prediction_text=f"Error: {e}", color_class='error')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
